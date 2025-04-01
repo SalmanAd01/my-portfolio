80c3432405b015768dc5619a08346a1ca7862424
SalmanAd01 <salmanadhikari415@gmail.com>
2025-04-01 22:10:39 +0530

    updated position

--- components/Resume/data.ts
+++ components/Resume/data.ts
@@ -2,7 +2,16 @@ import { ResumeOrderedDataItem } from "@/@types/types";
 
 const orderedData: ResumeOrderedDataItem[] = [
   {
-    key: "2024 Jul - Present",
+    key: "2025 Mar - 2025 Apr",
+    value: [
+      {
+        title: "Associate Software Developer - II",
+        content: "nVent",
+      },
+    ],
+  },
+  {
+    key: "2024 Jul - 2025 Mar",
     value: [
       {
         title: "Trainee Technologist",
